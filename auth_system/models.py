8b062bea441cf4e8aabe5409efe6d2178e853e6a
hellkirl <kirillprivalov04@icloud.com>
2023-07-30 19:49:22 +0300

    mypy fix

--- auth_system/models.py
+++ auth_system/models.py
@@ -1,5 +1,5 @@
 from config import DATABASE, PASSWORD, PORT, USERNAME
-from migrations.db import Connector
+from migrations.db import Connector  # type: ignore
 from pydantic.dataclasses import dataclass
 
 c = Connector(database=DATABASE, username=USERNAME, password=PASSWORD, port=PORT)
@@ -20,10 +20,19 @@ class User:
             cur.execute(
                 """INSERT INTO users (first_name, last_name, email, phone_number, login, password)
                                         VALUES (%s, %s, %s, %s, %s, %s)""",
-                (self.first_name, self.last_name, self.email, self.phone_number, self.login, self.password),
+                (
+                    self.first_name,
+                    self.last_name,
+                    self.email,
+                    self.phone_number,
+                    self.login,
+                    self.password,
+                ),
             )
         except Exception as error:
-            print(f"It is not possible to create a new user because of the following error {str(error)}")
+            print(
+                f"It is not possible to create a new user because of the following error {str(error)}"
+            )
             con.rollback()
 
     def check(self):
